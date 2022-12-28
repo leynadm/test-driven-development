f063957a2c576dba594fc6e1f1b2f026af801b6f
Leynad <matei_daniel@outlook.com>
2022-12-28 11:19:03 +0100

    Add the first function to capitalize first character in string

---
+++ capitalize.js
@@ -0,0 +1,32 @@
+function capitalize(string) {
+
+  let stringArr = string.split('')
+
+  let newString = '';
+
+  let element;
+
+  for (let index = 0; index < stringArr.length; index++) {
+      
+      if(index === 0){
+      
+          element = stringArr[index].toUpperCase();
+          
+          newString = newString + element
+
+      } else {
+
+          element = stringArr[index].toLowerCase()
+      
+          newString = newString + element
+  
+      }
+
+
+  }
+
+  return newString;
+
+}
+
+module.exports = capitalize;
---
+++ capitalize.test.js
@@ -0,0 +1,25 @@
+const capitalize = require('./capitalize');
+
+test('Capitalize first character in string', () => {
+  expect(capitalize("me")).toBe("Me");
+});
+
+
+test('Capitalize first character when there are two words', () => {
+  expect(capitalize("mE TODAY")).toBe("Me today");
+});
+
+
+test('Capitalize first character in string when follows a non-letter', () => {
+  expect(capitalize("1 me")).toBe("1 me");
+});
+
+
+test('Capitalize first character even when there are multiple words', () => {
+  expect(capitalize("ye ye ye ye")).toBe("Ye ye ye ye");
+});
+
+
+test('Capitalize first character in string even when full-case', () => {
+  expect(capitalize("DGSASDD")).toBe("Dgsasdd");
+});
